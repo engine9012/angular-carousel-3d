921e33cd394e83afd8ff1648f1e6d1e2aec38b32
jingren <jing.ren@huilianyi.com>
2018-03-13 19:38:59 +0800

    修复幻灯片闪屏的问题

--- dist/carousel-3d.js
+++ dist/carousel-3d.js
@@ -406,8 +406,7 @@
 
         var carousel3d = {
             restrict: 'AE',
-            template: '' + '<div class="carousel-3d-container" ng-switch="vm.isLoading" ng-mouseenter="vm.autoRotationLocked=true" ng-mouseleave="vm.autoRotationLocked=false">' +
-            '   <div class="carousel-3d-loader" ng-switch-when="true">' + '   </div>' + '   <div ng-switch-when="false" ng-switch="vm.isSuccessful">' + '       <div class="carousel-3d" ng-switch-when="true" ng-show="vm.isRendered" ng-transclude>' + '       </div>' + '       <p ng-switch-when="false" class="carousel-3d-loader-error">There was a problem during load</p>' + '       <div ng-if="vm.controls" class="carousel-3d-controls">' + '           <div class="carousel-3d-next arrow-left" ng-click="vm.options.dir === \'ltr\' ? vm.goNext() : vm.goPrev()"></div>' + '           <div class="carousel-3d-prev arrow-right" ng-click="vm.options.dir === \'ltr\' ? vm.goPrev() : vm.goNext()"></div>' + '       </div>' + '   </div>' + '</div>',
+            template: '' + '<div class="carousel-3d-container" ng-mouseenter="vm.autoRotationLocked=true" ng-mouseleave="vm.autoRotationLocked=false" style="height: 290px;">' + '   <div ng-switch="vm.isSuccessful">' + '       <div class="carousel-3d" ng-switch-when="true" ng-show="vm.isRendered" ng-transclude>' + '       </div>' + '       <p ng-switch-when="false" class="carousel-3d-loader-error">There was a problem during load</p>' + '       <div ng-if="vm.controls" class="carousel-3d-controls">' + '           <div class="carousel-3d-next arrow-left" ng-click="vm.options.dir === \'ltr\' ? vm.goNext() : vm.goPrev()"></div>' + '           <div class="carousel-3d-prev arrow-right" ng-click="vm.options.dir === \'ltr\' ? vm.goPrev() : vm.goNext()"></div>' + '       </div>' + '   </div>' + '</div>',
             replace: true,
             scope: {
                 model: '=ngModel',
